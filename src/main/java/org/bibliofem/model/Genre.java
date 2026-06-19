94686cd110c88638a6dfa88d2590fc1ad859e7f4
Kressala <chdimaio@protonmail.com>
2026-06-19 13:41:51 +0200

    Model classes and respective getters & setters created

--- src/main/java/org/bibliofem/model/Genre.java
+++ src/main/java/org/bibliofem/model/Genre.java
@@ -1,4 +1,33 @@
 package org.bibliofem.model;
 
 public class Genre {
+    private int idGenre;
+    private String genre;
+
+    public Genre(){
+
+    }
+
+    public Genre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getIdGenre() {
+        return idGenre;
+    }
+
+    public void setIdGenre(int idGenre) {
+        this.idGenre = idGenre;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 }
+
+
+
