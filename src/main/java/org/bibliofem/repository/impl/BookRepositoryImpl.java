5171a30b8b9ebf9089b0306d4d51325f1547c37b
alvi103-png <alvi103@gmail.com>
2026-06-23 09:53:07 +0200

    add: SQL query into BookRepositoryImpl

--- src/main/java/org/bibliofem/repository/impl/BookRepositoryImpl.java
+++ src/main/java/org/bibliofem/repository/impl/BookRepositoryImpl.java
@@ -5,15 +5,14 @@ import org.bibliofem.model.Book;
 import org.bibliofem.repository.BookRepository;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BookRepositoryImpl implements BookRepository {
     Connection connection;
-    PreparedStatement stmn;
-    ResultSet result;
+    Statement stmn;
 
     @Override
     public List<Book> getAllBooks(){
@@ -24,22 +23,35 @@ public class BookRepositoryImpl implements BookRepository {
                     b.isbn,
                     b.title,
                     b.description,
-                    a.author,
+                    STRING_AGG(a.author, ',') AS authors,
                     g.genre
                 FROM books b
                 JOIN book_author ba ON b.id = ba.book_id
                 JOIN authors a ON ba.author_id = a.id
                 JOIN genres g ON b.genre_id = g.id
+                GROUP BY b.id, b.isbn, b.title, b.description, g.genre
                 """;
         try {
             connection = DatabaseConnection.getConnection();
+            stmn = connection.createStatement();
+            ResultSet result = stmn.executeQuery(sql);
 
-
-
+            while (result.next()) {
+                Book book = new Book(
+                        result.getInt("id"),
+                        result.getString("isbn"),
+                        result.getString("title"),
+                        result.getString("description"),
+                        result.getString("genre"),
+                        result.getString("authors")
+                );
+                books.add(book);
+            }
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
-        } finally {
-            DatabaseConnection.closeConnection();
         }
+        finally {
+            DatabaseConnection.closeConnection();
+        } return books;
     }
 }
