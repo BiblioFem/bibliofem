45ed743296f8dfc32c7a1cadb74d0d11e7f8aab4
SiuzannaVach <siuzannavachaganian@gmail.com>
2026-06-23 11:02:35 +0200

    feat:add controller

--- src/main/java/org/bibliofem/controller/BookController.java
+++ src/main/java/org/bibliofem/controller/BookController.java
@@ -1,4 +1,17 @@
 package org.bibliofem.controller;
+import org.bibliofem.model.Book;
+import org.bibliofem.service.BookService;
+import java.util.List;
 
 public class BookController {
+    private BookService service;
+    public BookController (BookService service) {
+        this.service = service;
+
+    }
+    public List<Book> getAllBooks(){
+        return service.getAllBooks();
+    }
+
 }
+
